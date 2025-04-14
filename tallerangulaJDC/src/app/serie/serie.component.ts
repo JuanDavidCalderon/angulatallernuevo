7923240e82c3e133fa73b397cbea91fbcdc6601c
Juan David Calderon <jd.calderong12@uniandes.edu.co>
2025-04-13 21:58:24 -0500

    promedio

--- tallerangulaJDC/src/app/serie/serie.component.ts
+++ tallerangulaJDC/src/app/serie/serie.component.ts
@@ -12,10 +12,16 @@ import { series } from './datos';
 export class SerieComponent implements OnInit {
 
   series: Array<Serie> = [];
+  temporadasPromedio: string = '';
   constructor() { }
   
   getSerieList(): Array<Serie> {
     return series;
+  }
+
+  getTemporadasPromedio(series: Serie[]): string {
+    const totalTemporadas = series.reduce((sum, s) => sum + s.temporadas, 0);
+    return `Temporadas Promedio: ${totalTemporadas / series.length}`;
   }
 
 
@@ -24,6 +30,9 @@ export class SerieComponent implements OnInit {
 
   ngOnInit() {
     this.series = this.getSerieList();
+    this.temporadasPromedio=this.getTemporadasPromedio(this.series)
+
+    
     
   }
 
